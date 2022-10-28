24ead03fa9e81f436c9def69d40e0b460a0dc5b2
Sang Shin <sangshinpassion@gmail.com>
2022-10-28 10:17:38 -0400

    change

--- src/main/java/music_album/InMemoryCatalog.java
+++ src/main/java/music_album/InMemoryCatalog.java
@@ -15,25 +15,25 @@ import java.util.List;
 public class InMemoryCatalog implements ICatalog {
 
     private List<MusicItem> catalogOfMusicItems = new ArrayList<>(List.of(
-                   /* id    title                        artist                       releaseDate  price  musicCategory */
-        new MusicItem(1L,  "Diva",                      "Annie Lennox",              "1992-01-04", 13.99, MusicCategory.POP),
-        new MusicItem(2L,  "Dream of the Blue Turtles", "Sting",                     "1985-02-05", 14.99, MusicCategory.POP),
-        new MusicItem(3L,  "Trouble is...",             "Kenny Wayne Shepherd Band", "1997-08-08", 14.99, MusicCategory.BLUES),
-        new MusicItem(4L,  "Lie to Me",                 "Jonny Lang",                "1997-08-26", 17.97, MusicCategory.BLUES),
-        new MusicItem(5L,  "Little Earthquakes",        "Tori Amos",                 "1992-01-18", 14.99, MusicCategory.ALTERNATIVE),
-        new MusicItem(6L,  "Seal",                      "Seal",                      "1991-08-18", 17.97, MusicCategory.POP),
-        new MusicItem(7L,  "Ian Moore",                 "Ian Moore",                 "1993-12-05",  9.97, MusicCategory.CLASSICAL),
-        new MusicItem(8L,  "So Much for the Afterglow", "Everclear",                 "1997-01-19", 13.99, MusicCategory.ROCK),
-        new MusicItem(9L,  "Surfacing",                 "Sarah McLachlan",           "1997-12-04", 17.97, MusicCategory.ALTERNATIVE),
-        new MusicItem(10L, "Hysteria",                  "Def Leppard",               "1987-06-20", 17.97, MusicCategory.ROCK),
-        new MusicItem(11L, "A Life of Saturdays",       "Dexter Freebish",           "2000-12-06", 16.97, MusicCategory.RAP),
-        new MusicItem(12L, "Human Clay",                "Creed",                     "1999-10-21", 18.97, MusicCategory.ROCK),
-        new MusicItem(13L, "My, I'm Large",             "Bobs",                      "1987-02-20", 11.97, MusicCategory.COUNTRY),
-        new MusicItem(14L, "So",                        "Peter Gabriel",             "1986-10-03", 17.97, MusicCategory.POP),
-        new MusicItem(15L, "Big Ones",                  "Aerosmith",                 "1994-05-08", 18.97, MusicCategory.ROCK),
-        new MusicItem(16L, "90125",                     "Yes",                       "1983-10-16", 11.97, MusicCategory.ROCK),
-        new MusicItem(17L, "1984",                      "Van Halen",                 "1984-08-19", 11.97, MusicCategory.ROCK),
-        new MusicItem(18L, "Escape",                    "Journey",                   "1981-02-25", 11.97, MusicCategory.CLASSIC_ROCK))
+            /* id    title                        artist                       releaseDate  price  musicCategory */
+            new MusicItem(1L, "Diva", "Annie Lennox", "1992-01-04", 13.99, MusicCategory.POP),
+            new MusicItem(2L, "Dream of the Blue Turtles", "Sting", "1985-02-05", 14.99, MusicCategory.POP),
+            new MusicItem(3L, "Trouble is...", "Kenny Wayne Shepherd Band", "1997-08-08", 14.99, MusicCategory.BLUES),
+            new MusicItem(4L, "Lie to Me", "Jonny Lang", "1997-08-26", 17.97, MusicCategory.BLUES),
+            new MusicItem(5L, "Little Earthquakes", "Tori Amos", "1992-01-18", 14.99, MusicCategory.ALTERNATIVE),
+            new MusicItem(6L, "Seal", "Seal", "1991-08-18", 17.97, MusicCategory.POP),
+            new MusicItem(7L, "Ian Moore", "Ian Moore", "1993-12-05", 9.97, MusicCategory.CLASSICAL),
+            new MusicItem(8L, "So Much for the Afterglow", "Everclear", "1997-01-19", 13.99, MusicCategory.ROCK),
+            new MusicItem(9L, "Surfacing", "Sarah McLachlan", "1997-12-04", 17.97, MusicCategory.ALTERNATIVE),
+            new MusicItem(10L, "Hysteria", "Def Leppard", "1987-06-20", 17.97, MusicCategory.ROCK),
+            new MusicItem(11L, "A Life of Saturdays", "Dexter Freebish", "2000-12-06", 16.97, MusicCategory.RAP),
+            new MusicItem(12L, "Human Clay", "Creed", "1999-10-21", 18.97, MusicCategory.ROCK),
+            new MusicItem(13L, "My, I'm Large", "Bobs", "1987-02-20", 11.97, MusicCategory.COUNTRY),
+            new MusicItem(14L, "So", "Peter Gabriel", "1986-10-03", 17.97, MusicCategory.POP),
+            new MusicItem(15L, "Big Ones", "Aerosmith", "1994-05-08", 18.97, MusicCategory.ROCK),
+            new MusicItem(16L, "90125", "Yes", "1983-10-16", 11.97, MusicCategory.ROCK),
+            new MusicItem(17L, "1984", "Van Halen", "1984-08-19", 11.97, MusicCategory.ROCK),
+            new MusicItem(18L, "Escape", "Journey", "1981-02-25", 11.97, MusicCategory.CLASSIC_ROCK))
     );
 
     // TODO-01a - Write code below
@@ -108,74 +108,94 @@ public class InMemoryCatalog implements ICatalog {
 
 
     /**
-     * TODO-07: find all "rock" items whose price is less
-     * than or equal to the specified price.
+     * TODO-07:
+     * - Find all "rock" items whose price is less
+     *   than or equal to the specified price.
+     * - Write client code in the "InMemoryCatalogClient"
      */
 
 
     /**
-     * TODO-08: how many items of the specified genre
-     * (MusicCategory) do we sell?
+     * TODO-08:
+     * - Write code to find out how many items of the specified genre
+     *   (MusicCategory) do we sell?
+     * - Write client code in the "InMemoryCatalogClient"
      */
 
 
     /**
-     * TODO-09: determine average price of our low-cost,
-     * extensive catalog of music.
+     * TODO-09:
+     * - Compute average price of our low-cost,
+     *   extensive catalog of music.
+     * - Write client code in the "InMemoryCatalogClient"
      */
 
 
     /**
-     * TODO-10: find the cheapest item with the
-     * specified genre (MusicCategory).
+     * TODO-10:
+     * - Find the cheapest item with the
+     *   specified genre (MusicCategory)
+     * - Write client code in the "InMemoryCatalogClient".
      */
 
 
     /**
-     * TODO-11: find the average price of items in
-     * the specified genre (MusicCategory).
+     * TODO-11:
+     * - Find the average price of items in
+     *   the specified genre (MusicCategory).
+     * - Write client code in the "InMemoryCatalogClient".
      */
 
 
     /**
-     * TODO-12: are all items priced at least $10?
-     * This is a yes/no answer.
+     * TODO-12:
+     * - Find out if all items priced at least $10?
+     *   This is a yes/no answer.
+     * - Write client code in the "InMemoryCatalogClient".
      */
 
 
     /**
-     * TODO-13: do we sell any items with the
-     * specified genre (MusicCategory)?
-     * Another yes/no answer.
+     * TODO-13:
+     * - Find out if we sell any items with the
+     *   specified genre (MusicCategory)?
+     *   Another yes/no answer.
+     * - Write client code in the "InMemoryCatalogClient".
      */
 
 
     /**
-     * TODO-14: find the titles of all "pop" items,
-     * sorted by natural order.
-     * Just the titles!
+     * TODO-14:
+     * - Find the titles of all "pop" items,
+     *   sorted by natural order.
+     *   Just the titles!
+     * - Write client code in the "InMemoryCatalogClient".
      */
 
 
     /**
-     * TODO-15: find all items released in the 80s whose
-     * price is less than or equal to the specified price.
+     * TODO-15:
+     * - Find all items released in the 80s whose
+     *   price is less than or equal to the specified price.
+     * - Write client code in the "InMemoryCatalogClient".
      */
 
 
     /**
-     * TODO-16: return a map whose keys are all the
-     * genres (categories), and each key's associated value
-     * is a collection of items in that genre.
-     * If there is a genre that we don't currently
-     * sell, that key's associated value should
-     * be an empty collection, not null.
+     * TODO-16: (Do this task after you learn Map and HashMap)
+     * - Return a Map object whose keys are all the
+     *   genres (categories), and each key's associated value
+     *   is a collection of items in that genre.
+     *   If there is a genre that we don't currently
+     *   sell, that key's associated value should
+     *   be an empty collection, not null.
+     *   Write client code in the "InMemoryCatalogClient".
      */
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder(getClass().getSimpleName() + ": \n");
-        for (MusicItem item: catalogOfMusicItems) {
+        for (MusicItem item : catalogOfMusicItems) {
             builder.append(item).append("\n");
         }
         return builder.toString();
